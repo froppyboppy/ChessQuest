ca9331d2a40705406adee13e21374ce5dce4d396
Javier Cardoso <Cardoso.Javier@outlook.com>
2024-03-18 02:39:06 -0500

    Knight legal movements added

--- XR Chess/Assets/Scripts/ChessPieces/Knight.cs
+++ XR Chess/Assets/Scripts/ChessPieces/Knight.cs
@@ -4,4 +4,96 @@ using UnityEngine;
 
 public class Knight : ChessPiece
 {
+    public override List<Vector2Int> GetAvailableMoves(ref ChessPiece[,] board, int tileCountX, int tileCountY)
+    {
+        // valid moves list
+        List<Vector2Int> returnMoves = new List<Vector2Int>();
+
+        // Top right
+        int x = currentX + 1;
+        int y = currentY + 2;
+
+        if (x < tileCountX && y < tileCountY)
+        {
+            if (board[x, y] == null || board[x, y].team != null)
+            {
+                returnMoves.Add(new Vector2Int(x, y));
+            }
+        }
+
+        x = currentX + 2;
+        y = currentY + 1;
+        if (x < tileCountX && y < tileCountY)
+        {
+            if (board[x, y] == null || board[x, y].team != team)
+            {
+                returnMoves.Add(new Vector2Int(x, y));
+            }
+        }
+
+        // Top left
+        x = currentX - 1;
+        y = currentY + 2;
+        if (x >= 0 && y < tileCountY)
+        {
+            if (board[x, y] == null || board[x, y].team != team)
+            {
+                returnMoves.Add(new Vector2Int(x, y));
+            }
+        }
+
+        x = currentX - 2;
+        y = currentY + 1;
+        if (x >= 0 && y < tileCountY)
+        {
+            if (board[x, y] == null || board[x, y].team != team)
+            {
+                returnMoves.Add(new Vector2Int(x, y));
+            }
+        }
+
+        // Bottom right
+        x = currentX + 1;
+        y = currentY - 2;
+        if (x < tileCountX && y >= 0)
+        {
+            if (board[x, y] == null || board[x, y].team != team)
+            {
+                returnMoves.Add(new Vector2Int(x, y));
+            }
+        }
+
+        x = currentX + 2;
+        y = currentY - 1;
+        if (x < tileCountX && y >= 0)
+        {
+            if (board[x, y] == null || board[x, y].team != team)
+            {
+                returnMoves.Add(new Vector2Int(x, y));
+            }
+        }
+
+        // Bottom left
+        x = currentX - 1;
+        y = currentY - 2;
+        if (x >= 0 && y >= 0)
+        {
+            if (board[x, y] == null || board[x, y].team != team)
+            {
+                returnMoves.Add(new Vector2Int(x, y));
+            }
+        }
+
+        x = currentX - 2;
+        y = currentY - 1;
+        if (x >= 0 && y >= 0)
+        {
+            if (board[x, y] == null || board[x, y].team != team)
+            {
+                returnMoves.Add(new Vector2Int(x, y));
+            }
+        }
+
+        return returnMoves;
+    }
 }
--- XR Chess/Assets/Scripts/ChessPieces/Rook.cs
+++ XR Chess/Assets/Scripts/ChessPieces/Rook.cs
@@ -1,6 +1,102 @@
+using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
 
 public class Rook : ChessPiece
 {
+    public override List<Vector2Int> GetAvailableMoves(ref ChessPiece[,] board, int tileCountX, int tileCountY)
+    {
+        // valid moves list
+        List<Vector2Int> returnMoves = new List<Vector2Int>();
 
+        // Down
+        for (int i = currentY - 1; i >= 0; i--)
+        {
+            // valid move
+            if (board[currentX, i] == null)
+            {
+                returnMoves.Add(new Vector2Int(currentX, i));
+            }
+
+            // attack move
+            if (board[currentX, i] != null)
+            {
+                if (board[currentX, i].team != team)
+                {
+                    returnMoves.Add(new Vector2Int(currentX, i));
+                }
+
+                // stop when theres another piece
+                break;
+            }
+        }
+
+        // Up
+        for (int i = currentY + 1; i < tileCountY; i++)
+        {
+            // valid move
+            if (board[currentX, i] == null)
+            {
+                returnMoves.Add(new Vector2Int(currentX, i));
+            }
+
+            // attack move
+            if (board[currentX, i] != null)
+            {
+                if (board[currentX, i].team != team)
+                {
+                    returnMoves.Add(new Vector2Int(currentX, i));
+                }
+
+                // stop when theres another piece
+                break;
+            }
+        }
+
+        // Left
+        for (int i = currentX - 1; i >= 0; i--)
+        {
+            // valid move
+            if (board[i, currentY] == null)
+            {
+                returnMoves.Add(new Vector2Int(i, currentY));
+            }
+
+            // attack move
+            if (board[i, currentY] != null)
+            {
+                if (board[i, currentY].team != team)
+                {
+                    returnMoves.Add(new Vector2Int(i, currentY));
+                }
+
+                // stop when theres another piece
+                break;
+            }
+        }
+
+        // Right
+        for (int i = currentX + 1; i < tileCountX; i++)
+        {
+            // valid move
+            if (board[i, currentY] == null)
+            {
+                returnMoves.Add(new Vector2Int(i, currentY));
+            }
+
+            // attack move
+            if (board[i, currentY] != null)
+            {
+                if (board[i, currentY].team != team)
+                {
+                    returnMoves.Add(new Vector2Int(i, currentY));
+                }
+
+                // stop when theres another piece
+                break;
+            }
+        }
+
+        return returnMoves;
+    }
 }
